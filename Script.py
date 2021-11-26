1391deafec7ce98fc9ecfdd0c35a5d7089261316
shiva20991 <80451254+shiva20991@users.noreply.github.com>
2021-11-26 08:31:47 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,7 +12,7 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
  
-- Source - https//:google.com 
+- Source - Dont be smart 
 
 <b>DEVS:</b>
 - <a href=https://t.me/tamilblasters_win</a>"""
